ef8e0bfb984a3d6c4659f8feb7363d1cb755324f
Fauzi0309 <fauzirahmandhani@gmail.com>
2023-10-25 14:00:38 +0700

    FEAT:menambahkan fitur atau file baru

---
+++ repository/girlGroup.js
@@ -0,0 +1,8 @@
+const artistsAndSongs = {
+    "fromis_9": ["DM", "FUN"],
+    "Blackpink": ["How You Like That"],
+    "JKT48": ["Rapsodi", "Heavy Rotation"],
+    "Twice": ["What is Love?"],
+}
+
+console.log(artistsAndSongs);
